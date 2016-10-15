63dde318ce737115148f6f765363bf94774fbda3
BerlianVerenita <berlian_vebiola_23rpl@student.smktelkom-mlg.sch.id>
2016-10-15 16:17:10 +0700

    Menambahkan variable dan inisialisasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4008/formpendaftarananggota/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl4008/formpendaftarananggota/MainActivity.java
@@ -2,14 +2,37 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl4008.formpendaftarananggota;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    RadioGroup rgJK;
+    CheckBox cbMbb, cbMnl, cbMgg, cbMgr;
+    Spinner spKelas;
+    Button bOk;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        rgJK = (RadioGroup) findViewById(R.id.radioGroupJK);
+        cbMbb = (CheckBox) findViewById(R.id.checkBoxMbb);
+        cbMnl = (CheckBox) findViewById(R.id.checkBoxMnl);
+        cbMgg = (CheckBox) findViewById(R.id.checkBoxMgg);
+        cbMgr = (CheckBox) findViewById(R.id.checkBoxMgr);
+        spKelas = (Spinner) findViewById(R.id.spinnerKelas);
+
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
     }
 }
